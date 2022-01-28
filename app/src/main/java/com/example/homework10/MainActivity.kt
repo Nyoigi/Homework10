c9f38d6da1a0282ca525dbb48e0e8d9e6b7c44af
Nyoigi <67091699+Nyoigi@users.noreply.github.com>
2022-01-28 16:06:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework10/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.homework10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnAction.setOnClickListener {
+            chooseImage()
+        }
+    }
+
+    private fun chooseImage() {
+        when (etSearchPicture.text.toString()) {
+            "Щенок" -> ivImageOutput.setBackgroundResource(R.drawable.puppy)
+            "Хорек" -> ivImageOutput.setBackgroundResource(R.drawable.hor)
+            "Капибара" -> ivImageOutput.setBackgroundResource(R.drawable.capibara)
+            "Пейзаж" -> ivImageOutput.setBackgroundResource(R.drawable.beautiful)
+        }
+    }
+}
